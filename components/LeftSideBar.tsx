4d208fcbbaf7359d5558d552cce92fc4532c1e5a
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-08 14:23:54 +0530

    added public assests

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const LeftSideBar = () => {
   return (
-    <div>LeftSideBar</div>
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link href='/'>
+          <Image src="/icons/logo.svg" alt='logo' width={23} height={27} />
+        </Link>
+      </nav>
+    </section>
   )
 }
 

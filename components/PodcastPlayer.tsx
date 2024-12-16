fe0d2115e7279b9225230d86ac8afc409d0ce208
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-16 12:05:13 +0530

    Podcaster player working corretly

--- components/PodcastPlayer.tsx
+++ components/PodcastPlayer.tsx
@@ -10,13 +10,20 @@ import { useAudio } from "@/providers/AudioProvider";
 import { Progress } from "./ui/progress";
 
 const PodcastPlayer = () => {
+
+  // audio context
+  const { audio } = useAudio();
+  
+  // audio ref 
   const audioRef = useRef<HTMLAudioElement>(null);
+
+  // states
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(0);
   const [isMuted, setIsMuted] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
-  const { audio } = useAudio();
 
+  // play-pause toggle func
   const togglePlayPause = () => {
     if (audioRef.current?.paused) {
       audioRef.current?.play();
@@ -27,6 +34,7 @@ const PodcastPlayer = () => {
     }
   };
 
+  // mute toggle func
   const toggleMute = () => {
     if (audioRef.current) {
       audioRef.current.muted = !isMuted;
@@ -34,17 +42,23 @@ const PodcastPlayer = () => {
     }
   };
 
+  // skip forward 5s
   const forward = () => {
+
+    // checks due to TS
     if (
       audioRef.current &&
       audioRef.current.currentTime &&
       audioRef.current.duration &&
+
+      // otherwise just this will do
       audioRef.current.currentTime + 5 < audioRef.current.duration
     ) {
       audioRef.current.currentTime += 5;
     }
   };
 
+  // skip back 5s
   const rewind = () => {
     if (audioRef.current && audioRef.current.currentTime - 5 > 0) {
       audioRef.current.currentTime -= 5;
@@ -53,6 +67,7 @@ const PodcastPlayer = () => {
     }
   };
 
+  // update current time
   useEffect(() => {
     const updateCurrentTime = () => {
       if (audioRef.current) {
@@ -61,6 +76,8 @@ const PodcastPlayer = () => {
     };
 
     const audioElement = audioRef.current;
+
+    // timeupdate event listener to always update the time
     if (audioElement) {
       audioElement.addEventListener("timeupdate", updateCurrentTime);
 
@@ -70,6 +87,7 @@ const PodcastPlayer = () => {
     }
   }, []);
 
+  // allows to play-pause audio
   useEffect(() => {
     const audioElement = audioRef.current;
     if (audio?.audioURL) {
@@ -83,12 +101,15 @@ const PodcastPlayer = () => {
       setIsPlaying(true);
     }
   }, [audio]);
+
+  // sets the duration from audio ref to state
   const handleLoadedMetadata = () => {
     if (audioRef.current) {
       setDuration(audioRef.current.duration);
     }
   };
 
+  // sets state to false when audio ends 
   const handleAudioEnded = () => {
     setIsPlaying(false);
   };
@@ -100,12 +121,17 @@ const PodcastPlayer = () => {
       })}
     >
       {/* change the color for indicator inside the Progress component in ui folder */}
+      {/* progress bar */}
       <Progress
         value={(currentTime / duration) * 100}
         className="w-full"
         max={duration}
       />
+
+      {/* Player body */}
       <section className="glassmorphism-black flex h-[112px] w-full items-center justify-between px-4 max-md:justify-center max-md:gap-5 md:px-12">
+
+        {/* audio provider */}
         <audio
           ref={audioRef}
           src={audio?.audioURL}
@@ -113,7 +139,11 @@ const PodcastPlayer = () => {
           onLoadedMetadata={handleLoadedMetadata}
           onEnded={handleAudioEnded}
         />
+
+        {/* audio info */}
         <div className="flex items-center gap-4 max-md:hidden">
+
+          {/* podcast thumbnail with link to podcast */}
           <Link href={`/podcast/${audio?.podcastId}`}>
             <Image
               src={audio?.imageURL! || "/images/player1.png"}
@@ -123,6 +153,8 @@ const PodcastPlayer = () => {
               className="aspect-square rounded-xl"
             />
           </Link>
+
+          {/* podcast title & author */}
           <div className="flex w-[160px] flex-col">
             <h2 className="text-14 truncate font-semibold text-white-1">
               {audio?.title}
@@ -130,7 +162,10 @@ const PodcastPlayer = () => {
             <p className="text-12 font-normal text-white-2">{audio?.author}</p>
           </div>
         </div>
+
+        {/* controls */}
         <div className="flex-center cursor-pointer gap-3 md:gap-6">
+          {/* icon for rewind */}
           <div className="flex items-center gap-1.5">
             <Image
               src={"/icons/reverse.svg"}
@@ -141,6 +176,8 @@ const PodcastPlayer = () => {
             />
             <h2 className="text-12 font-bold text-white-4">-5</h2>
           </div>
+
+          {/* play-pause icon */}
           <Image
             src={isPlaying ? "/icons/Pause.svg" : "/icons/Play.svg"}
             width={30}
@@ -148,6 +185,8 @@ const PodcastPlayer = () => {
             alt="play"
             onClick={togglePlayPause}
           />
+
+          {/* icon for forward */}
           <div className="flex items-center gap-1.5">
             <h2 className="text-12 font-bold text-white-4">+5</h2>
             <Image
@@ -159,10 +198,14 @@ const PodcastPlayer = () => {
             />
           </div>
         </div>
+
+        {/*  */}
         <div className="flex items-center gap-6">
           <h2 className="text-16 font-normal text-white-2 max-md:hidden">
             {formatTime(duration)}
           </h2>
+
+          {/* icon for muting */}
           <div className="flex w-full gap-2">
             <Image
               src={isMuted ? "/icons/unmute.svg" : "/icons/mute.svg"}
